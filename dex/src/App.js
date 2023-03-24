6d3341d3531b927e2a8ce18beb07f665eef729b3
DEFIProtocol <Beauallgood@gmail.com>
2023-03-24 11:30:05 -0500

    fixing paths

--- dex/src/App.js
+++ dex/src/App.js
@@ -13,11 +13,11 @@ function App() {
     <Header connect={connect} isConnect={isConnected} address={address} />
     <div className="mainWindow">
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route exact path="/tokens" element={<Tokens />} />
         <Route exact path="/admin" element={<Admin />} /> 
         <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />    
-        <Route path="/swap" element={<Swap isConnect={isConnected} address={address}/>} />
+        <Route exact path="/swap" element={<Swap isConnect={isConnected} address={address}/>} />
         <Route exact path="/:name?/:uuid?" element={<TokenDetails address={address} />} />
         <Route exact path="coins/:name?/:uuid?" element={<CryptoDetails address={address} />} />
       </Routes>
