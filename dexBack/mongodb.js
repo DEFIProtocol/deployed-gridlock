f5dbd3650f9ac62b6dc40232c62801d383a63e9a
DEFIProtocol <Beauallgood@gmail.com>
2023-09-28 19:58:06 -0500

    adding port data

--- dexBack/mongodb.js
+++ dexBack/mongodb.js
@@ -102,7 +102,7 @@ app.use(express.json());
 //    res.status(500).json({ error: 'Proxying request failed' });
 //  }
 //});
-//const PORT = process.env.port || 3005;
+const PORT = process.env.port || 3005;
 
 
 // Define API endpoints
