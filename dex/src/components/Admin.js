98f25b997a1b7e7f89bb281ac90e1a0cb43b6c6a
DEFIProtocol <Beauallgood@gmail.com>
2023-10-01 18:19:34 -0500

    updating frontend: remove axiosHeaders

--- dex/src/components/Admin.js
+++ dex/src/components/Admin.js
@@ -1,91 +1,56 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, useMemo } from 'react';
 import { useGetCryptosQuery } from './services/cryptoApi';
-import { Row, Col, Card, Button } from "antd";
-import { Loader } from "./elements";
+import { Row, Col, Card, Button } from 'antd';
+import { Loader } from './elements';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
-import "./tokenIndex.css";
+import './tokenIndex.css';
 
 function Admin() {
-  const [count, setCount] = useState("100")
+  const [count, setCount] = useState('100');
   const { data, isFetching } = useGetCryptosQuery(count);
   const cryptos = data?.data?.coins;
   const chains = useMemo(
-    () => ["1","56","137","10","42161","100","43114","250","8217","1313161554"],
+    () => ['1', '56', '137', '10', '42161', '100', '43114', '250', '8217', '1313161554'],
     []
   );
-  const chainNames = ["Ethereum","BNB","Polygon","Optimism","Arbitrum","Gnosis","Avalanche","Fantom","Klaytn","Aurora"]
-  const axiosHeaders = {
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.REACT_APP_1INCH_API_KEY}`
-    }
-  };
+  const chainNames = ['Ethereum', 'BNB', 'Polygon', 'Optimism', 'Arbitrum', 'Gnosis', 'Avalanche', 'Fantom', 'Klaytn', 'Aurora'];
   const [chainResponses, setChainResponses] = useState({});
   const [selectedChain, setSelectedChain] = useState(null);
 
   useEffect(() => {
-    // Define the fetch1InchTokens function inside the useEffect callback
-    async function fetch1InchTokens(chain) {
-      try {
-        console.log(`${process.env.REACT_APP_BACKEND}/api/1inch/token/v1.2/${chain}?provider=1inch&country=US`)
-        const response = await fetch(
-          `${process.env.REACT_APP_BACKEND}/api/1inch/token/v1.2/${chain}?provider=1inch&country=US`,
-          axiosHeaders
-        );
-        const data = await response.json();
-        console.log(data);
-        return data;
-      } catch (error) {
-        throw error;
-      }
-    }
-  
     // Fetch data for each chain when the component mounts
     async function fetchDataForChains() {
       const responses = {};
-  
+
       for (const chain of chains) {
         try {
-          const response = await fetch1InchTokens(chain);
-          responses[chain] = response;
+          console.log(`${process.env.REACT_APP_BACKEND}/api/1inch/token/v1.2/${chain}?provider=1inch&country=US`);
+          const response = await fetch(`${process.env.REACT_APP_BACKEND}/api/1inch/token/v1.2/${chain}?provider=1inch&country=US`);
+          const data = await response.json();
+          console.log(data);
+          responses[chain] = data;
         } catch (error) {
           console.error(`Error fetching data for chain ${chain}:`, error);
         }
       }
-  
+
       setChainResponses(responses);
     }
-  
+
     fetchDataForChains();
   }, [chains]); // Include 'chains' as a dependency
-  
-  console.log(chainResponses)
 
-  async function fetch1InchTokens(chain) {
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_BACKEND}/api/1inch/token/v1.2/${chain}?provider=1inch&country=US`,
-        axiosHeaders
-      );
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  console.log(fetch1InchTokens())
+  console.log(chainResponses);
 
   const handleChainButtonClick = (chain) => {
     setSelectedChain(chain);
   };
 
-  if(isFetching) return <Loader />;
+  if (isFetching) return <Loader />;
   console.log(cryptos);
 
-   return (
+  return (
     <>
       <div className="chain-buttons">
         {chainNames.map((chainName, index) => (
@@ -98,26 +63,26 @@ function Admin() {
           </Button>
         ))}
       </div>
-      <Row gutter={[32,32]} className ="crypto-card-container">
+      <Row gutter={[32, 32]} className="crypto-card-container">
         <input value="e" onChange={(e) => setCount(e.target.value)} />
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg ={6} className ='crypto-card'>
-            <Link key={currency.uuid} to = {`/crypto/${currency.uuid}`}>
-              <Card 
-                title = {`${currency.rank}.${currency.name}`}
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+            <Link to={`/crypto/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}.${currency.name}`}
                 extra={<img className="logo" src={currency.iconUrl} alt="no logo" />}
                 hoverable
-                >
-                  <p>Price: {millify(currency.price)}</p>
-                  <p>Price: {millify(currency.marketCap)}</p>
-                  <p>Price: {millify(currency.change)}</p>
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Price: {millify(currency.marketCap)}</p>
+                <p>Price: {millify(currency.change)}</p>
               </Card>
             </Link>
           </Col>
         ))}
       </Row>
     </>
-  )
+  );
 }
 
 export default Admin;
--- dex/src/components/elements/MarketOrder.js
+++ dex/src/components/elements/MarketOrder.js
@@ -1,90 +1,79 @@
 import React, { useState, useEffect } from 'react';
-import "./order.css";
+import './order.css';
 import tokenList from '../../tokenList';
-import { Popover, Radio, message, Modal } from "antd";
-import { SettingOutlined, } from "@ant-design/icons";
+import { Popover, Radio, message, Modal } from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
 import axios from 'axios';
-import { useSendTransaction, useWaitForTransaction } from "wagmi";
-import { useEthereum } from ".";
+import { useSendTransaction, useWaitForTransaction } from 'wagmi';
+import { useEthereum } from '.';
 
 function MarketOrder(props) {
-  const { uuid, address, usdPrice, tokenName } = props
-  const tokenObject= tokenList.find((token) => token.uuid === uuid);
+  const { uuid, address, usdPrice, tokenName } = props;
+  const tokenObject = tokenList.find((token) => token.uuid === uuid);
   const { cryptoDetails } = useEthereum();
-  const [messageApi, contextHolder] = message.useMessage()
+  const [messageApi, contextHolder] = message.useMessage();
   const [amount, setAmount] = useState(null);
   const [slippage, setSlippage] = useState(2.5);
   const [txDetailsSent, setTxDetailsSent] = useState(false);
-  const ethAddress = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
-  const ethExRate = usdPrice / cryptoDetails?.price
-  const baseLine = "1";
-  const [checked, setChecked] = useState("eth")
+  const ethAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+  const ethExRate = usdPrice / cryptoDetails?.price;
+  const baseLine = '1';
+  const [checked, setChecked] = useState('eth');
   const [txDetails, setTxDetails] = useState({
     to: null,
     data: null,
     value: null,
-  })
-  const {data, sendTransaction}= useSendTransaction({
+  });
+  const { data, sendTransaction } = useSendTransaction({
     request: {
       from: address,
       to: String(txDetails.to),
       data: String(txDetails.data),
       value: String(txDetails.value),
-    }
-  })
-  const {isLoading, isSuccess} = useWaitForTransaction({
-    hash: data?.hash
-  })
+    },
+  });
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
 
-
-  function handleSlippageChange(e){
-    setSlippage(e.taget.value);
+  function handleSlippageChange(e) {
+    setSlippage(e.target.value);
   }
-  
-  const axiosHeaders = {
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.REACT_APP_1INCH_API_KEY}`
-    }
-  };
-  
+
   async function fetchDexBuy() {
     const tokenAmount = () => {
-      if (checked === "eth") {
+      if (checked === 'eth') {
         return String(amount * (baseLine.padEnd(18 + baseLine.length, '0')));
-      } else if (checked === "usd") {
+      } else if (checked === 'usd') {
         return String((amount / cryptoDetails.price) * (baseLine.padEnd(18 + baseLine.length, '0')));
       } else {
-        return String((amount * ethExRate) * (baseLine.padEnd(18 + baseLine.length, "0")));
+        return String((amount * ethExRate) * (baseLine.padEnd(18 + baseLine.length, '0')));
       }
     };
-  
+
     try {
       // Check allowance and fetch quote
       const allowanceResponse = await axios.get(
-        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/allowance?tokenAddress=${ethAddress}&walletAddress=${address}`,
-        axiosHeaders
+        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/allowance?tokenAddress=${ethAddress}&walletAddress=${address}`
       );
       const allowance = allowanceResponse.data;
-  
-      if (allowance.allowance === "0") {
+
+      if (allowance.allowance === '0') {
         const approveResponse = await axios.get(
-          `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/transaction?tokenAddress=${ethAddress}&amount=${tokenAmount()}`,
-          axiosHeaders
+          `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/transaction?tokenAddress=${ethAddress}&amount=${tokenAmount()}`
         );
         setTxDetails(approveResponse.data);
-        console.log("Not Approved");
+        console.log('Not Approved');
         return;
       }
-  
+
       // Fetch quote for buy
       const quoteResponse = await axios.get(
-        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/quote?src=${ethAddress}&dst=${tokenObject.address}&amount=${tokenAmount()}&fee=1.25&includeTokensInfo=true&includeGas=true`,
-        axiosHeaders
+        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/quote?src=${ethAddress}&dst=${tokenObject.address}&amount=${tokenAmount()}&fee=1.25&includeTokensInfo=true&includeGas=true`
       );
 
       console.log(quoteResponse);
-  
+
       // Show a confirmation modal
       Modal.confirm({
         title: 'Confirm Buy',
@@ -92,17 +81,16 @@ function MarketOrder(props) {
         onOk: async () => {
           // If confirmed, proceed with the swap
           const txResponse = await axios.get(
-            `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/swap?src=${ethAddress}&dst=${tokenObject.address}&amount=${tokenAmount()}&from=${address}&slippage=${slippage}&fee=1.25&referrer=${process.env.REACT_APP_ADMIN_ADDRESS}&receiver=${address}`,
-            axiosHeaders
+            `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/swap?src=${ethAddress}&dst=${tokenObject.address}&amount=${tokenAmount()}&from=${address}&slippage=${slippage}&fee=1.25&referrer=${process.env.REACT_APP_ADMIN_ADDRESS}&receiver=${address}`
           );
-  
+
           let decimals = Number(`1E${tokenObject.decimals}`);
           let tokenTwo = (Number(txResponse.data.toTokenAmount) / decimals).toFixed(2);
           console.log(tokenTwo);
           return setTxDetails(txResponse.data.tx);
         },
         onCancel: () => {
-          console.log("Buy Cancelled"); // Handle cancel action
+          console.log('Buy Cancelled'); // Handle cancel action
         },
       });
     } catch (error) {
@@ -110,45 +98,41 @@ function MarketOrder(props) {
       // Handle the error appropriately
     }
   }
-  
-  
+
   async function fetchDexSell() {
     const tokenSellAmount = () => {
-      if (checked === "eth") {
+      if (checked === 'eth') {
         return String(Math.trunc(amount / ethExRate * (baseLine.padEnd(tokenObject.decimals + baseLine.length, '0'))));
-      } else if (checked === "usd") {
+      } else if (checked === 'usd') {
         return String(Math.trunc((amount / usdPrice) * (baseLine.padEnd(tokenObject.decimals + baseLine.length, '0'))));
       } else {
-        return String(amount * (baseLine.padEnd(tokenObject.decimals + baseLine.length, "0")));
+        return String(amount * (baseLine.padEnd(tokenObject.decimals + baseLine.length, '0')));
       }
     };
-  
+
     try {
       // Check allowance and fetch quote
       const allowanceResponse = await axios.get(
-        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/allowance?tokenAddress=${tokenObject.address}&walletAddress=${address}`,
-        axiosHeaders
+        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/allowance?tokenAddress=${tokenObject.address}&walletAddress=${address}`
       );
       const allowance = allowanceResponse.data;
-  
-      if (allowance.allowance === "0") {
+
+      if (allowance.allowance === '0') {
         const approveResponse = await axios.get(
-          `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/transaction?tokenAddress=${tokenObject.address}&amount=${tokenSellAmount()}`,
-          axiosHeaders
+          `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/approve/transaction?tokenAddress=${tokenObject.address}&amount=${tokenSellAmount()}`
         );
         setTxDetails(approveResponse.data);
-        console.log("Not Approved");
+        console.log('Not Approved');
         return;
       }
-  
+
       // Fetch quote for sell
       const quoteResponse = await axios.get(
-        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/quote?src=${tokenObject.address}&dst=${ethAddress}&amount=${tokenSellAmount()}&fee=1.25&includeTokensInfo=true&includeGas=true`,
-        axiosHeaders
+        `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/quote?src=${tokenObject.address}&dst=${ethAddress}&amount=${tokenSellAmount()}&fee=1.25&includeTokensInfo=true&includeGas=true`
       );
-      
+
       console.log(quoteResponse);
-  
+
       // Show a confirmation modal
       Modal.confirm({
         title: 'Confirm Sell',
@@ -156,17 +140,16 @@ function MarketOrder(props) {
         onOk: async () => {
           // If confirmed, proceed with the swap
           const txResponse = await axios.get(
-            `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/swap?src=${tokenObject.address}&dst=${ethAddress}&amount=${tokenSellAmount()}&from=${address}&slippage=${slippage}&fee=1.25&referrer=${process.env.REACT_APP_ADMIN_ADDRESS}&receiver=${address}`,
-            axiosHeaders
+            `${process.env.REACT_APP_BACKEND}/api/1inch/swap/v5.2/1/swap?src=${tokenObject.address}&dst=${ethAddress}&amount=${tokenSellAmount()}&from=${address}&slippage=${slippage}&fee=1.25&referrer=${process.env.REACT_APP_ADMIN_ADDRESS}&receiver=${address}`
           );
-  
+
           let decimals = Number(`1E${tokenObject.decimals}`);
           let tokenTwo = (Number(txResponse.data.toTokenAmount) / decimals).toFixed(2);
           console.log(tokenTwo);
           return setTxDetails(txResponse.data.tx);
         },
         onCancel: () => {
-          console.log("Sell Cancelled"); // Handle cancel action
+          console.log('Sell Cancelled'); // Handle cancel action
         },
       });
     } catch (error) {
@@ -174,11 +157,10 @@ function MarketOrder(props) {
       // Handle the error appropriately
     }
   }
-  
-  
 
   useEffect(() => {
-    if (txDetails.to && address && !txDetailsSent) { // Add a check for txDetailsSent
+    if (txDetails.to && address && !txDetailsSent) {
+      // Add a check for txDetailsSent
       sendTransaction();
       setTxDetailsSent(true); // Set txDetailsSent to true to avoid repeated calls
     }
@@ -186,107 +168,102 @@ function MarketOrder(props) {
 
   useEffect(() => {
     messageApi.destroy();
-    if(isLoading){
+    if (isLoading) {
       messageApi.open({
         type: 'loading',
         content: 'Transaction is Pending...',
         duration: 0,
-      })
+      });
     }
-  }, [isLoading, messageApi])
+  }, [isLoading, messageApi]);
 
   useEffect(() => {
     messageApi.destroy();
-    if(isSuccess){
+    if (isSuccess) {
       messageApi.open({
         type: 'success',
         content: 'Transaction Successful',
         duration: 1.5,
-      })
-    }else if (txDetails.to){
+      });
+    } else if (txDetails.to) {
       messageApi.open({
-        type: 'error', 
+        type: 'error',
         content: 'Transaction Failed',
-        duration: 1.5
-      })
+        duration: 1.5,
+      });
     }
-  }, [isSuccess, messageApi, txDetails.to])
+  }, [isSuccess, messageApi, txDetails.to]);
 
-  console.log(txDetails)
+  console.log(txDetails);
 
   const settings = (
     <>
       <div>Slippage Tolerance</div>
       <div>
-        <Radio.Group value={slippage} onChange={handleSlippageChange} >
+        <Radio.Group value={slippage} onChange={handleSlippageChange}>
           <Radio.Button value={0.5}>0.5%</Radio.Button>
           <Radio.Button value={2.5}>2.5%</Radio.Button>
           <Radio.Button value={5}>5.0%</Radio.Button>
         </Radio.Group>
       </div>
     </>
-  )
-  
+  );
+
   return (
     <>
-    {contextHolder}
+      {contextHolder}
       <div className="checkboxContainer">
         <div className="checkboxes">
-        <input
-          type="checkbox"
-          name="Priced in Eth"
-          value="eth"
-          checked={checked === "eth" ? checked : !checked}
-          onChange={(e) => setChecked(e.target.value)}
+          <input
+            type="checkbox"
+            name="Priced in Eth"
+            value="eth"
+            checked={checked === 'eth' ? checked : !checked}
+            onChange={(e) => setChecked(e.target.value)}
           />
-        <label htmlFor="ethEx" style={{ color: 'DarkGray' }}>
-          {' '}
-          Order Priced in ETH
-        </label>
-        <br />
-        <input
-          type="checkbox"
-          name="Priced in USD"
-          value="usd"
-          checked={checked === "usd" ? checked : null}
-          onChange={(e) => setChecked(e.target.value)}
+          <label htmlFor="ethEx" style={{ color: 'DarkGray' }}>
+            {' '}
+            Order Priced in ETH
+          </label>
+          <br />
+          <input
+            type="checkbox"
+            name="Priced in USD"
+            value="usd"
+            checked={checked === 'usd' ? checked : null}
+            onChange={(e) => setChecked(e.target.value)}
           />
-        <label htmlFor="usdEx" style={{ color: 'DarkGray' }}>
-          {' '}
-          Order Priced in USD
-        </label>
-        <br />
-        <input
-          type="checkbox"
-          name="Priced in USD"
-          value={`${tokenName}`}
-          checked={checked === tokenName ? checked : null}
-          onChange={(e) => setChecked(e.target.value)}
+          <label htmlFor="usdEx" style={{ color: 'DarkGray' }}>
+            {' '}
+            Order Priced in USD
+          </label>
+          <br />
+          <input
+            type="checkbox"
+            name="Priced in USD"
+            value={`${tokenName}`}
+            checked={checked === tokenName ? checked : null}
+            onChange={(e) => setChecked(e.target.value)}
           />
-        <label htmlFor="usdEx" style={{ color: 'DarkGray' }}>
-          {' '}
-          Order Priced in {tokenName}
-        </label>
+          <label htmlFor="usdEx" style={{ color: 'DarkGray' }}>
+            {' '}
+            Order Priced in {tokenName}
+          </label>
         </div>
-        <Popover 
-        content={settings}
-        title="settings"
-        trigger="click"
-        placement="bottomLeft"
-      >
-        <SettingOutlined className="cog" />
-      </Popover>
+        <Popover content={settings} title="settings" trigger="click" placement="bottomLeft">
+          <SettingOutlined className="cog" />
+        </Popover>
       </div>
       <input type="text" placeholder="Amount" className="input" onChange={(e) => setAmount(e.target.value)} />
       <div className="buttonContainer">
-        <button className="buyButton" type="button" placeholder="Buy" value="buy" disabled={!amount || !address } onClick={fetchDexBuy} >
+        <button className="buyButton" type="button" placeholder="Buy" value="buy" disabled={!amount || !address} onClick={fetchDexBuy}>
           Buy
         </button>
-        <button className="sellButton" type="button" placeholder="Sell" value="sell" disabled={!amount || !address } onClick={fetchDexSell} >
+        <button className="sellButton" type="button" placeholder="Sell" value="sell" disabled={!amount || !address} onClick={fetchDexSell}>
           Sell
-        </button>  
+        </button>
       </div>
-      <div style={{color: "lime", fontWeight: '700', margin: "0px auto", padding: "4%"}}>
+      <div style={{ color: 'lime', fontWeight: '700', margin: '0px auto', padding: '4%' }}>
         Eth Price per Token = {ethExRate}
       </div>
     </>
