29ce7e3d01c18587886802ed21e9414f71c95ff0
DEFIProtocol <Beauallgood@gmail.com>
2023-12-26 21:06:22 -0600

    adding polygon and binance

--- dex/src/components/Admin.js
+++ dex/src/components/Admin.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { useGetCryptosQuery } from './services/cryptoApi';
-import { Row, Col, Card, Button } from 'antd';
+import { Row, Card, Button } from 'antd';
 import { Loader } from './elements';
 import axios from 'axios';
 import millify from 'millify';
@@ -8,8 +8,8 @@ import { Link } from 'react-router-dom';
 import './tokenIndex.css';
 
 function Admin() {
-  const [count, setCount] = useState('100');
-  const { data, isFetching } = useGetCryptosQuery(count);
+  const [query, setQuery] = useState("");
+  const { data, isFetching } = useGetCryptosQuery('1200');
   const cryptos = data?.data?.coins;
   const chains = useMemo(
     () => ['1', '56', '137', '10', '42161', '100', '43114', '250', '8217', '1313161554'],
@@ -44,47 +44,81 @@ function Admin() {
       console.error(error);
     }
   }
-  console.log(chainResponses);
-
   const handleChainButtonClick = (chain) => {
     setSelectedChain(chain);
   };
+  console.log(chainResponses)
+  const arrayOfObjects = chainResponses === null ? null : JSON.stringify(Object.entries(chainResponses).map(([key, value]) => ({ 
+    address: value.address,
+    symbol: value.symbol,
+    name: value.name,
+  })))
+  console.log(arrayOfObjects)
 
   if (isFetching) return <Loader />;
   console.log(cryptos);
 
   return (
-    <>
+    <div style = {{width: "100%"}}>
+    <Row>
       <div className="chain-buttons">
         {chainNames.map((chainName, index) => (
           <Button
-            key={chainName}
-            type={selectedChain === chains[index] ? 'primary' : 'default'}
-            onClick={() => handleChainButtonClick(chains[index])}
+          key={chainName}
+          type={selectedChain === chains[index] ? 'primary' : 'default'}
+          onClick={() => handleChainButtonClick(chains[index])}
           >
             {chainName}
           </Button>
         ))}
       </div>
-      <Row gutter={[32, 32]} className="crypto-card-container">
-        <input value="e" onChange={(e) => setCount(e.target.value)} />
-        {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
-            <Link to={`/crypto/${currency.uuid}`}>
-              <Card
-                title={`${currency.rank}.${currency.name}`}
-                extra={<img className="logo" src={currency.iconUrl} alt="no logo" />}
-                hoverable
-              >
-                <p>Price: {millify(currency.price)}</p>
-                <p>Market Cap: {millify(currency.marketCap)}</p>
-                <p>Change: {millify(currency.change)}</p>
-              </Card>
-            </Link>
-          </Col>
-        ))}
       </Row>
-    </>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        <div style={{ textAlign: "center" }}>
+        <input
+          placeholder="Search..."
+          className="searchBar"
+          type="text"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+        {/*<input value={count} onChange={(e) => setCount(e.target.value)} />*/}
+        {!cryptos
+          ? null
+          : cryptos
+              .filter((val) => {
+                if (query === "") {
+                  return cryptos;
+                } else {
+                  return val?.name.toLowerCase().includes(query.toLowerCase()) 
+                  //  && val.Symbol.toLowerCase().includes(query.toLowerCase());
+                }
+              })
+              .map((token, index) => (
+                <div key={index}>
+                    <Card className="daoCard">
+                      <div className="cardContainer" >
+                        {index}
+                        <Link to={`/${token?.name}/${token?.uuid}`}>
+                          <img className="logo" src={token.iconUrl} alt="noLogo" />
+                          <div style={{ float: "right" }}>
+                            <h4 className="name">{token.name}</h4>
+                            <span className="symbol">{token.symbol}</span>
+                          </div>
+                        </Link>
+                        <Link to={`/${token?.name}/${token?.uuid}`} className="type">
+                            {token.marketCap == null ? "--" : millify(token.marketCap)}
+                        </Link>
+                        <Link to={`/${token?.name}/${token?.uuid}`} className="lastPrice">
+                            {token.price == null ? "--" : millify(token.price)}
+                        </Link>
+                        <span className="symbol">{token.uuid}</span>
+                      </div>
+                  </Card>
+                </div>
+              ))}
+      </Row>
+    </div>
   );
 }
 
